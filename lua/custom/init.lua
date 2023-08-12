9dc20004a4ef68be3aa2ed155d7c77db63ab2de1
Sovereign Shahid <sovereignshahid@gmail.com>
2023-08-11 18:33:32 -0600

    Removed Powershell because 🤢

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,7 +1,1 @@
 vim.wo.relativenumber = true
-vim.cmd[[
-  let &shell = has('win32') ? 'powershell' : 'pwsh'
-  set shellquote= shellpipe=\| shellxquote=
-  set shellcmdflag=-NoLogo\ -NoProfile\ -ExecutionPolicy\ RemoteSigned\ -Command
-  set shellredir=\|\ Out-File\ -Encoding\ UTF8
-]]
