f0a6e92f73cee16f1454949cb52019e1ad6892d2
Sovereign Shahid <s.shahid1@share.epsb.ca>
2023-10-05 10:15:02 -0600

    Set up LuaSnip

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,4 +1,4 @@
-local lsp_zero = require('lsp-zero')
+local lsp_zero = require('lsp-zero').preset('recommended')
 lsp_zero.extend_lspconfig()
 
 require 'lspconfig'.lua_ls.setup {
@@ -16,13 +16,70 @@ lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.buffer_autoformat()
 end)
 
+lsp_zero.setup()
+
+
+
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
+require('luasnip.loaders.from_vscode').lazy_load()
+
+local ls = require("luasnip")
+-- some shorthands...
+local snip = ls.snippet
+local node = ls.snippet_node
+local text = ls.text_node
+local insert = ls.insert_node
+local func = ls.function_node
+local choice = ls.choice_node
+local dynamicn = ls.dynamic_node
+
+local date = function() return { os.date('%Y-%m-%d') } end
+
+ls.add_snippets(nil, {
+  all = {
+    snip({
+      trig = "date",
+      namr = "Date",
+      dscr = "Date in the form of YYYY-MM-DD",
+    }, {
+      func(date, {}),
+    }),
+  },
+  python = {
+    snip({
+        trig = "info",
+        namr = "Info Metadata",
+        dscr = "Info metadata for python files"
+      },
+      {
+        text({ "\"\"\"",
+          "Title: " }), insert(1, "note_title"), text({ "",
+        "Author: Soverign Shahid" }), text({ "",
+        "Date: " }), func(date, {}), text({ "", "\"\"\"" })
+      }),
+    snip({
+      trig = "main",
+      namr = "Main Code Block",
+      dscr = "creates if name == main block"
+    }, {
+      text({ "if __name__ = \"__main__\":", "\t" })
+    })
+  }
+})
 
 cmp.setup({
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+    { name = 'luasnip' }, -- For luasnip users.
+  }),
   mapping = cmp.mapping.preset.insert({
-    ['<Tab>'] = cmp_action.tab_complete(),
-    ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
-    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+    ['<Tab>'] = cmp_action.luasnip_supertab(),
+    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
   })
 })
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -27,11 +27,7 @@ require("lazy").setup({
     config = function()
       require 'nvim-treesitter.configs'.setup {
         -- A list of parser names, or "all" (the five listed parsers should always be installed)
-        ensure_installed = { "python", "lua", "vim", "vimdoc", "rust", "c", "cpp", "markdown" }
-      }
-      require 'nvim-treesitter.configs'.setup {
-        -- A list of parser names, or "all" (the five listed parsers should always be installed)
-        ensure_installed = { "python", "lua", "vim", "vimdoc", "rust", "c", "cpp", "markdown" }
+        ensure_installed = { "python", "lua", "vim", "vimdoc", "rust", "c", "cpp", "markdown", "latex" }
       }
     end
   },
@@ -70,17 +66,6 @@ require("lazy").setup({
     dependencies = 'nvim-cmp'
   },
   {
-    "L3MON4D3/LuaSnip",
-    lazy = false,
-    config = function()
-      require("config.luasnip")
-    end
-  },
-  {
-    'saadparwaiz1/cmp_luasnip',
-    dependencies = "L3MON4D3/LuaSnip"
-  },
-  {
     'williamboman/mason.nvim',
     init = function()
       require("mason").setup({
@@ -100,7 +85,7 @@ require("lazy").setup({
     config = function()
       local lsp_zero = require('lsp-zero')
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua-ls", "rust-analyzer", "python-lsp-server" },
+        ensure_installed = { "lua_ls", "rust_analyzer", "pylsp", "texlab" },
         automatic_installation = true,
         handlers = {
           lsp_zero.default_setup,
@@ -112,96 +97,11 @@ require("lazy").setup({
     "tpope/vim-fugitive",
     lazy = false,
   },
-  "nvim-lua/plenary.nvim",
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.2',
-    lazy = false,
-    dependencies = 'nvim-lua/plenary.nvim',
-    config = function()
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    end
-  },
-  "neovim/nvim-lspconfig",
-  {
-    'hrsh7th/nvim-cmp',
-    config = function()
-      require('config.nvim-cmp')
-    end
-  },
-  {
-    'hrsh7th/cmp-nvim-lsp',
-    dependencies = 'nvim-cmp'
-  },
-  {
-    'hrsh7th/cmp-buffer',
-    dependencies = 'nvim-cmp'
-  },
-  {
-    'hrsh7th/cmp-path',
-    dependencies = 'nvim-cmp'
-  },
-  {
-    'hrsh7th/cmp-path',
-    dependencies = 'nvim-cmp'
-  },
-  {
-    'hrsh7th/cmp-cmdline',
-    dependencies = 'nvim-cmp'
-  },
-  --{
-  --  "L3MON4D3/LuaSnip",
-  --  lazy = false,
-  --  config = function()
-  --    require("config.luasnip")
-  --  end
-  --},
-  --{
-  -- 'saadparwaiz1/cmp_luasnip',
-  --  dependencies = "L3MON4D3/LuaSnip",
-  --},
-  {
-    'williamboman/mason.nvim',
-    init = function()
-      require("mason").setup({
-        ui = {
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-          }
-        }
-      })
-    end
-  },
-  {
-    'williamboman/mason-lspconfig.nvim',
-    dependencies = { 'williamboman/mason.nvim', 'VonHeikemen/lsp-zero.nvim' },
-    config = function()
-      local lsp_zero = require('lsp-zero')
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "rust_analyzer" },
-        handlers = {
-          lsp_zero.default_setup,
-        },
-      })
-    end
-  },
-  {
-    "tpope/vim-fugitive",
-    lazy = false,
-    config = function()
-      require("config.fugitive")
-    end
-  },
   {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v3.x',
-    dependencies = 'hrsh7th/nvim-cmp',
+    dependencies = { 'hrsh7th/nvim-cmp' },
+    lazy = false,
     config = function()
       require("config.lsp")
     end
