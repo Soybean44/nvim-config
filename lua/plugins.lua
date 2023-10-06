68010c5eb8fc39887993c6a7d20284f8ea8364e3
Sovereign Shahid <sovereignshahid@gmail.com>
2023-10-06 08:14:52 -0600

    fixed luasnip stuff again

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,190 +1,184 @@
 require("lazy").setup({
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		lazy = false,
-		init = function()
-			require("catppuccin").setup({
-				flavour = "mocha",
-				transparent_background = true,
-			})
-			vim.cmd.colorscheme("catppuccin")
-		end,
-	},
-	"nvim-tree/nvim-web-devicons",
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = "nvim-tree/nvim-web-devicons",
-		lazy = false,
-		config = function()
-			require("config.lualine")
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		lazy = false,
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				-- A list of parser names, or "all" (the five listed parsers should always be installed)
-				ensure_installed = { "python", "lua", "vim", "vimdoc", "rust", "c", "cpp", "markdown", "latex" },
-			})
-		end,
-	},
-	"nvim-lua/plenary.nvim",
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.2",
-		lazy = false,
-		dependencies = "nvim-lua/plenary.nvim",
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			require("config.lsp")
-		end,
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		version = "1.x",
-		config = function()
-			require("config.nvim-cmp")
-		end,
-	},
-	{
-		"hrsh7th/cmp-nvim-lsp",
-		dependencies = "nvim-cmp",
-	},
-	{
-		"hrsh7th/cmp-buffer",
-		dependencies = "nvim-cmp",
-	},
-	{
-		"hrsh7th/cmp-path",
-		dependencies = "nvim-cmp",
-	},
-	{
-		"hrsh7th/cmp-path",
-		dependencies = "nvim-cmp",
-	},
-	{
-		"hrsh7th/cmp-cmdline",
-		dependencies = "nvim-cmp",
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		config = function()
-			require("config.luasnip")
-		end,
-	},
-	{
-		"hrsh7th/cmp-luasnip",
-		dependencies = "nvim-cmp",
-	},
-	{
-		"williamboman/mason.nvim",
-		init = function()
-			require("mason").setup({
-				ui = {
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗",
-					},
-				},
-			})
-		end,
-	},
-	{
-		"stevearc/conform.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			local conform = require("conform")
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    lazy = false,
+    init = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = true,
+      })
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+  "nvim-tree/nvim-web-devicons",
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    lazy = false,
+    config = function()
+      require("config.lualine")
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        -- A list of parser names, or "all" (the five listed parsers should always be installed)
+        ensure_installed = { "python", "lua", "vim", "vimdoc", "rust", "c", "cpp", "markdown", "latex" },
+      })
+    end,
+  },
+  "nvim-lua/plenary.nvim",
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.2",
+    lazy = false,
+    dependencies = "nvim-lua/plenary.nvim",
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("config.lsp")
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    version = "1.x",
+  },
+  {
+    "hrsh7th/cmp-nvim-lsp",
+    dependencies = "nvim-cmp",
+  },
+  {
+    "hrsh7th/cmp-buffer",
+    dependencies = "nvim-cmp",
+  },
+  {
+    "hrsh7th/cmp-path",
+    dependencies = "nvim-cmp",
+  },
+  {
+    "hrsh7th/cmp-path",
+    dependencies = "nvim-cmp",
+  },
+  {
+    "hrsh7th/cmp-cmdline",
+    dependencies = "nvim-cmp",
+  },
+  {
+    "L3MON4D3/LuaSnip",
+  },
+  {
+    "saadparwaiz1/cmp_luasnip",
+    dependencies = { "nvim-cmp", "L3MON4D3/LuaSnip" },
+  },
+  {
+    "williamboman/mason.nvim",
+    init = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local conform = require("conform")
 
-			conform.setup({
-				formatters_by_ft = {
-					javascript = { "prettier" },
-					typescript = { "prettier" },
-					javascriptreact = { "prettier" },
-					typescriptreact = { "prettier" },
-					svelte = { "prettier" },
-					css = { "prettier" },
-					html = { "prettier" },
-					json = { "prettier" },
-					yaml = { "prettier" },
-					markdown = { "prettier" },
-					graphql = { "prettier" },
-					lua = { "stylua" },
-					python = { "isort", "black" },
-				},
-				format_on_save = {
-					lsp_fallback = true,
-					async = false,
-					timeout_ms = 500,
-				},
-			})
-			vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-				conform.format({
-					lsp_fallback = true,
-					async = false,
-					timeout_ms = 500,
-				})
-			end, { desc = "Format file or range (in visual mode)" })
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = { "williamboman/mason.nvim" },
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "rust_analyzer", "pylsp", "texlab" },
-				automatic_installation = true,
-			})
-		end,
-	},
-	{
-		"tpope/vim-fugitive",
-		lazy = false,
-	},
-	{
-		"nvim-neorg/neorg",
-		build = ":Neorg sync-parsers",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("neorg").setup({
-				load = {
-					["core.defaults"] = {}, -- Loads default behaviour
-					["core.concealer"] = {}, -- Adds pretty icons to your documents
-					["core.dirman"] = { -- Manages Neorg workspaces
-						config = {
-							workspaces = {
-								notes = "~/Documents/notes",
-							},
-						},
-					},
-				},
-			})
-		end,
-	},
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		init = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 300
-		end,
-		config = function()
-			require("config.whichkey")
-		end,
-	},
-	{
-		"akinsho/toggleterm.nvim",
-		config = true,
-	},
-	{
-		"lervag/vimtex",
-		config = function()
-			require("config.tex")
-		end,
-	},
+      conform.setup({
+        formatters_by_ft = {
+          javascript = { "prettier" },
+          typescript = { "prettier" },
+          javascriptreact = { "prettier" },
+          typescriptreact = { "prettier" },
+          svelte = { "prettier" },
+          css = { "prettier" },
+          html = { "prettier" },
+          json = { "prettier" },
+          yaml = { "prettier" },
+          markdown = { "prettier" },
+          graphql = { "prettier" },
+          lua = { "stylua" },
+          python = { "isort", "black" },
+        },
+        format_on_save = {
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 500,
+        },
+      })
+      vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+        conform.format({
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 500,
+        })
+      end, { desc = "Format file or range (in visual mode)" })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "rust_analyzer", "pylsp", "texlab" },
+        automatic_installation = true,
+      })
+    end,
+  },
+  {
+    "tpope/vim-fugitive",
+    lazy = false,
+  },
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {},  -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.dirman"] = {      -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/Documents/notes",
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    config = function()
+      require("config.whichkey")
+    end,
+  },
+  {
+    "akinsho/toggleterm.nvim",
+    config = true,
+  },
+  {
+    "lervag/vimtex",
+    config = function()
+      require("config.tex")
+    end,
+  },
 })
