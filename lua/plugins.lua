88990411fca0b0e3de2d7ab0971cc0263c8fb69d
Sovereign Shahid <sovereignshahid@gmail.com>
2023-11-05 14:09:51 -0700

    added spellcheck for latex

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -131,7 +131,7 @@ require("lazy").setup {
     dependencies = { "williamboman/mason.nvim" },
     config = function()
       require("mason-lspconfig").setup {
-        ensure_installed = { "lua_ls", "rust_analyzer", "pylsp", "texlab", "zls", "svelte" },
+        ensure_installed = { "lua_ls", "rust_analyzer", "pylsp", "texlab", "zls", "svelte", "ltex" },
         automatic_installation = true,
       }
     end,
