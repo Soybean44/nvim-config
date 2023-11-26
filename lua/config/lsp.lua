f3f6504f04057c9d6d83553ad0e5b6741146ea31
Sovereign Shahid <sovereignshahid@gmail.com>
2023-11-26 15:49:06 -0700

    fixed c/cpp support

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -16,6 +16,7 @@ lspconfig.zls.setup({})
 lspconfig.svelte.setup({})
 lspconfig.gopls.setup({})
 lspconfig.ltex.setup({})
+lspconfig.clangd.setup({})
 lspconfig.rust_analyzer.setup({
   -- Server-specific settings. See `:help lspconfig-setup`
   settings = {
