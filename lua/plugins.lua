a4127d0fd32e27a9e8591e909a5ae7b2180d0c6b
Sovereign Shahid <sovereignshahid@gmail.com>
2024-01-15 21:48:17 -0700

    attempted to add nvim dap

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -246,7 +246,22 @@ require("lazy").setup {
     end,
   },
   "kmonad/kmonad-vim",
-  "mfussenegger/nvim-dap",
+  {
+    "mfussenegger/nvim-dap",
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {},
+      ensure_installed = {
+        "codelldb",
+      }
+    }
+  },
   {
     "mfussenegger/nvim-dap-python",
     dependencies = "mfussenegger/nvim-dap",
@@ -255,22 +270,23 @@ require("lazy").setup {
       require("dap-python").setup "/home/soybean44/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
     end,
   },
-  --{
-  --  "rcarriga/nvim-dap-ui",
-  --  dependencies = "mfussenegger/nvim-dap",
-  --  config = function()
-  --    local dap, dapui = require "dap", require "dapui"
-  --    dap.listeners.after.event_initialized["dapui_config"] = function()
-  --      dapui.open()
-  --    end
-  --    dap.listeners.before.event_terminated["dapui_config"] = function()
-  --      dapui.close()
-  --    end
-  --    dap.listeners.before.event_exited["dapui_config"] = function()
-  --      dapui.close()
-  --    end
-  --  end,
-  --},
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
   {
     "tpope/vim-markdown",
     ft = "markdown",
