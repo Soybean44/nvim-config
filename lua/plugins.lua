e3b59bb2a31a193164808350a79b78beae590c28
Sovereign Shahid <s.shahid1@share.epsb.ca>
2024-02-06 12:39:22 -0700

    changed the keybinds so they dont interfere with zellij

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -373,26 +373,19 @@ require("lazy").setup {
         harpoon.ui:toggle_quick_menu(harpoon:list())
       end)
 
-      vim.keymap.set("n", "<C-h>", function()
+      vim.keymap.set("n", "<C-j>", function()
         harpoon:list():select(1)
       end)
-      vim.keymap.set("n", "<C-t>", function()
+      vim.keymap.set("n", "<C-f>", function()
         harpoon:list():select(2)
       end)
       vim.keymap.set("n", "<C-d>", function()
         harpoon:list():select(3)
       end)
-      vim.keymap.set("n", "<C-s>", function()
+      vim.keymap.set("n", "<C-k>", function()
         harpoon:list():select(4)
       end)
 
-      -- Toggle previous & next buffers stored within Harpoon list
-      vim.keymap.set("n", "<C-b>", function()
-        harpoon:list():prev()
-      end)
-      vim.keymap.set("n", "<C-f>", function()
-        harpoon:list():next()
-      end)
     end,
   },
   'ThePrimeagen/vim-be-good',
