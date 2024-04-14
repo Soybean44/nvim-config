53dee43e00f1f873c105eddb029b52ed5c27a97a
Sovereign Shahid <sovereignshahid@gmail.com>
2024-04-14 09:21:14 -0600

    fixed harpoon

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -364,7 +364,7 @@ require("lazy").setup {
       harpoon:setup()
 
       vim.keymap.set("n", "<leader>a", function()
-        harpoon:list():append()
+        harpoon:list():add()
       end)
       vim.keymap.set("n", "<C-e>", function()
         harpoon.ui:toggle_quick_menu(harpoon:list())
