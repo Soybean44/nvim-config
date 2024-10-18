ed6e11b682fa6419e23e24d829a9149ddd1dbf7f
Sovereign Shahid <sovereignshahid@gmail.com>
2024-10-18 17:46:16 -0600

    added format dissable for those pesky times when you dont want formatting

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -41,6 +41,13 @@ return {
             prepend_args = { "-A14", "-t" },
           },
         },
+        format_on_save = function(bufnr)
+          -- Disable with a global or buffer-local variable
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+          end
+          return { timeout_ms = 500, lsp_format = "fallback" }
+        end,
       }
       vim.keymap.set({ "n", "v" }, "<leader>mp", function()
         conform.format {
@@ -49,11 +56,22 @@ return {
           timeout_ms = 500,
         }
       end, { desc = "Format file or range (in visual mode)" })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = "*",
-        callback = function(args)
-          require("conform").format { bufnr = args.buf }
-        end,
+      vim.api.nvim_create_user_command("FormatDisable", function(args)
+        if args.bang then
+          -- FormatDisable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, {
+        desc = "Disable autoformat-on-save",
+        bang = true,
+      })
+      vim.api.nvim_create_user_command("FormatEnable", function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+      end, {
+        desc = "Re-enable autoformat-on-save",
       })
     end,
   },
