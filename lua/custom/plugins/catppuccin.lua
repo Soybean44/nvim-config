ad65c9b7e2908d92055485694ab7737ae1cf8d7d
Sovereign Shahid <sovereignshahid@gmail.com>
2024-10-25 19:38:25 -0600

    added neovide stuff to get it to work

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -1,21 +1,26 @@
-return   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    lazy = false,
-    init = function()
-      require("catppuccin").setup {
-        flavour = "mocha",
-        transparent_background = true,
-        highlight_overrides = {
-          mocha = function(mocha)
-            return {
-              Todo = { fg = mocha.yellow, bg = mocha.base },
-            }
-          end,
-        },
-      }
-      vim.cmd.colorscheme "catppuccin"
-    end,
-  }
-
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  lazy = false,
+  init = function()
+    local function get_bg()
+      if vim.g.neovide then
+        return false
+      end
+      return true
+    end
+    require("catppuccin").setup {
+      flavour = "mocha",
+      transparent_background = get_bg(),
+      highlight_overrides = {
+        mocha = function(mocha)
+          return {
+            Todo = { fg = mocha.yellow, bg = mocha.base },
+          }
+        end,
+      },
+    }
+    vim.cmd.colorscheme "catppuccin"
+  end,
+}
--- lua/options.lua
+++ lua/options.lua
@@ -1,44 +1,48 @@
 -- Hint: use `:h <option>` to figure out the meaning if needed
-vim.opt.clipboard = 'unnamedplus' -- use system clipboard
-vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
-vim.opt.mouse = 'a'               -- allow the mouse to be used in Nvim
+vim.opt.clipboard = "unnamedplus" -- use system clipboard
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+vim.opt.mouse = "a" -- allow the mouse to be used in Nvim
 
-vim.filetype.add({
-    extension = {
-        templ = "templ",
-    },
-})
+vim.filetype.add {
+  extension = {
+    templ = "templ",
+  },
+}
 
 vim.api.nvim_create_augroup("AsmGroup", { clear = true })
 
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
   group = "AsmGroup",
   pattern = { "*.asm", "*.s", "*.S" },
-  command = "set filetype=fasm"
+  command = "set filetype=fasm",
 })
 
 -- Tab
-vim.opt.tabstop = 2      -- number of visual spaces per TAB
-vim.opt.softtabstop = 2  -- number of spacesin tab when editing
-vim.opt.shiftwidth = 2   -- insert 4 spaces on a tab
+vim.opt.tabstop = 2 -- number of visual spaces per TAB
+vim.opt.softtabstop = 2 -- number of spacesin tab when editing
+vim.opt.shiftwidth = 2 -- insert 4 spaces on a tab
 vim.opt.expandtab = true -- tabs are spaces, mainly because of python
 
 -- UI config
-vim.opt.number = true         -- show absolute number
+vim.opt.number = true -- show absolute number
 vim.opt.relativenumber = true -- add numbers to each line on the left side
-vim.opt.cursorline = false    -- highlight cursor line underneath the cursor horizontally
-vim.opt.splitbelow = true     -- open new vertical split bottom
-vim.opt.splitright = true     -- open new horizontal splits right
+vim.opt.cursorline = false -- highlight cursor line underneath the cursor horizontally
+vim.opt.splitbelow = true -- open new vertical split bottom
+vim.opt.splitright = true -- open new horizontal splits right
 vim.opt.conceallevel = 1
+
+-- Neovide
+vim.g.neovide_scale_factor = 0.6
+
 -- vim.opt.termguicolors = true        -- enabl 24-bit RGB color in the TUI
 vim.opt.showmode = false -- we are experienced, wo don't need the "-- INSERT --" mode hint
 
 -- Searching
-vim.opt.incsearch = true  -- search as characters are entered
-vim.opt.hlsearch = false  -- do not highlight matches
+vim.opt.incsearch = true -- search as characters are entered
+vim.opt.hlsearch = false -- do not highlight matches
 vim.opt.ignorecase = true -- ignore case in searches by default
-vim.opt.smartcase = true  -- but make it case sensitive if an uppercase is entered
+vim.opt.smartcase = true -- but make it case sensitive if an uppercase is entered
 
 -- Spell Check
-vim.opt.spelllang = { 'en', 'cjk' }
-vim.opt.spellsuggest = { 'best', 9 }
+vim.opt.spelllang = { "en", "cjk" }
+vim.opt.spellsuggest = { "best", 9 }
