d93a25123ffca951e7204e52f70d8a530b2b06a1
Sovereign Shahid <sovereignshahid@gmail.com>
2024-11-21 13:17:41 -0700

    fixed love2d lua config

--- lua/custom/config/lsp.lua
+++ lua/custom/config/lsp.lua
@@ -3,13 +3,42 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protoc
 local lspconfig = require "lspconfig"
 lspconfig.lua_ls.setup {
   capabilities = capabilities,
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT'
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+          -- Depending on the usage, you might want to add additional paths here.
+          "~/git-repos/LLS-Addons/addons"
+          -- "${3rd}/busted/library",
+        }
+        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+        -- library = vim.api.nvim_get_runtime_file("", true)
+      }
+    })
+  end,
+
   settings = {
     Lua = {
       completion = {
         callSnippet = "Replace",
       },
       diagnostics = {
-        disable = { "unused-local", "undefined-field" },
+        disable = { "unused-local", "undefined-field", "lowercase-global" },
       },
       workspace = {
         userThirdParty = { "~/git-repos/LLS-Addons/addons" },
