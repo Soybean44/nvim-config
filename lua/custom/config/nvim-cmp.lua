c2ff0756432fffeb9973290c91d40e89f4250ca8
Sovereign Shahid <sovereignshahid@gmail.com>
2024-12-06 11:34:19 -0700

    changed order of nvim-cmp

--- lua/custom/config/nvim-cmp.lua
+++ lua/custom/config/nvim-cmp.lua
@@ -1,31 +1,31 @@
-local cmp = require('cmp')
-require('custom.config.luasnip')
-local ls = require('luasnip')
+local cmp = require "cmp"
+require "custom.config.luasnip"
+local ls = require "luasnip"
 
 local has_words_before = function()
   unpack = unpack or table.unpack
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
 end
 
-cmp.setup({
+cmp.setup {
   snippet = {
     expand = function(args)
       ls.lsp_expand(args.body)
-    end
+    end,
   },
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'buffer' },
-    { name = 'luasnip' }, -- For luasnip users.
-    { name = 'emmet_vim' },
-    { name = 'lazydev' },
-  }),
-  mapping = cmp.mapping.preset.insert({
-    ['<C-Space>'] = cmp.mapping.complete(),
+  sources = cmp.config.sources {
+    { name = "luasnip" }, -- For luasnip users.
+    { name = "nvim_lsp" },
+    { name = "buffer" },
+    { name = "emmet_vim" },
+    { name = "lazydev" },
+  },
+  mapping = cmp.mapping.preset.insert {
+    ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
     ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
     ["<C-y>"] = cmp.mapping.confirm { select = true },
     ["<Tab>"] = cmp.config.dissable,
-  })
-})
+  },
+}
