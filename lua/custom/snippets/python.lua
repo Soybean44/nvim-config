a30f50183fcdd810d3ccdeab8d12ad3f3f480ae9
Sovereign Shahid <sovereignshahid@gmail.com>
2024-12-09 14:21:13 -0700

    converted info snippet to fmt

--- lua/custom/snippets/python.lua
+++ lua/custom/snippets/python.lua
@@ -13,18 +13,26 @@ end
 
 ls.add_snippets(nil, {
   python = {
-    s({
-      trig = "info",
-      namr = "Info Metadata",
-      dscr = "Info metadata for python files",
-    }, {
-      t { '"""', "Title: " },
-      i(1, "note_title"),
-      t { "", "Author: Sovereign Shahid" },
-      t { "", "Date: " },
-      f(date, {}),
-      t { "", '"""' },
-    }),
+    s(
+      {
+        trig = "info",
+        namr = "Info Metadata",
+        dscr = "Info metadata for python files",
+      },
+      fmt(
+        [[
+        """
+        Title: {}
+        Author: Sovereign Shahid
+        Date: {}
+        """
+        ]],
+        {
+          i(1, "note_title"),
+          f(date, {}),
+        }
+      )
+    ),
     s({
       trig = "main",
       namr = "Main Code Block",
