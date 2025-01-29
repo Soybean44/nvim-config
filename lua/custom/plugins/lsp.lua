e4e8ec69ba7378a473538f95af7e3ab03424754b
Sovereign Shahid <sovereignshahid@gmail.com>
2025-01-29 15:06:05 -0700

    hide warnings in latex

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -83,6 +83,7 @@ return {
       vim.g.vimtex_view_general_viewer = "okular"
       vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
       vim.g.vimtex_compiler_method = "latexrun"
+      vim.g.vimtex_quickfix_ignore_filters = ["Warning:"]
     end,
     ft = "tex",
   },
