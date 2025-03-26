59afd17bd0618a25a8a952d20123a2665955f110
Sovereign Shahid <sovereignshahid@gmail.com>
2025-03-26 16:28:00 -0600

    removed ignore warnings cuz i cant see anything for latex

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -85,7 +85,6 @@ return {
       vim.g.vimtex_view_general_viewer = "okular"
       vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
       vim.g.vimtex_compiler_method = "tectonic"
-      vim.g.vimtex_quickfix_ignore_filters = { "Warning:" }
     end,
     ft = "tex",
   },
