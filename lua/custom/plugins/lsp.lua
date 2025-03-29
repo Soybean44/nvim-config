65665bb4bf0c65890f9deef45c4c44cf38ff56be
Sovereign Shahid <sovereignshahid@gmail.com>
2025-03-29 10:49:31 -0600

    added auto compilation

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -85,6 +85,7 @@ return {
       vim.g.vimtex_view_general_viewer = "okular"
       vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
       vim.g.vimtex_compiler_method = "latexmk"
+      vim.g.vimtex_auto_compile = 1
       vim.g.vimtex_latexmk = {
         backend = 'latexmk',
         options = {
