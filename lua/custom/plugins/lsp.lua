5e4b2de2ce47f55b66300f7be934dbb41611bb1a
Sovereign Shahid <sovereignshahid@gmail.com>
2025-03-29 10:29:30 -0600

    switched to latexmk so i can finally see error but now packages are gonna be a pain again

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -84,7 +84,14 @@ return {
       vim.g.maplocalleader = ","
       vim.g.vimtex_view_general_viewer = "okular"
       vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
-      vim.g.vimtex_compiler_method = "tectonic"
+      vim.g.vimtex_compiler_method = "latexmk"
+      vim.g.vimtex_latexmk = {
+        backend = 'latexmk',
+        options = {
+          '-pdf',
+          '-pdflatex=lualatex',
+        },
+      }
     end,
     ft = "tex",
   },
