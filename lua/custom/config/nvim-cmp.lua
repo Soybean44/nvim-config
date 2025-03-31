89241114d73644108b682d1c8b062365364c2460
Sovereign Shahid <sovereignshahid@gmail.com>
2025-03-31 09:39:35 -0600

    Removed autocomplete cuz it messed with snippets

--- lua/custom/config/nvim-cmp.lua
+++ lua/custom/config/nvim-cmp.lua
@@ -9,6 +9,9 @@ vim.opt.completeopt = { "menu", "noinsert", "noselect" }
 vim.opt.shortmess:append "c"
 
 cmp.setup {
+  completion = {
+    autocomplete = false
+  },
   snippet = {
     expand = function(args)
       ls.lsp_expand(args.body)
@@ -44,10 +47,10 @@ cmp.setup {
         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
         -- can also be a function to dynamically calculate max width such as
         -- menu = function() return math.floor(0.45 * vim.o.columns) end,
-        menu = 50, -- leading text (labelDetails)
-        abbr = 50, -- actual suggestion item
+        menu = 50,              -- leading text (labelDetails)
+        abbr = 50,              -- actual suggestion item
       },
-      ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+      ellipsis_char = "...",    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
       show_labelDetails = true, -- show labelDetails in menu. Disabled by default
 
       -- The function below will be called before any actual modifications from lspkind
