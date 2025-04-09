4934bceb9f49a8aef1bcd19f87eecfd26f9d5381
Sovereign Shahid <sovereignshahid@gmail.com>
2025-04-08 19:50:52 -0600

    changed notes directory

--- lua/custom/plugins/miscellaneous.lua
+++ lua/custom/plugins/miscellaneous.lua
@@ -15,7 +15,7 @@ return {
       })
       vim.g.vimwiki_list = {
         {
-          path = '~/Documents/notes',
+          path = '~/syncthing/notes',
           syntax = 'markdown',
           ext = 'md',
           index = 'index',
