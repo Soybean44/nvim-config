c527ec514477f1fb87054103d8b80a8548023975
Sovereign Shahid <sovereignshahid@gmail.com>
2025-04-24 15:04:49 -0600

    changed markdown image render to be on inline to make things easier to edit

--- lua/custom/plugins/miscellaneous.lua
+++ lua/custom/plugins/miscellaneous.lua
@@ -40,7 +40,7 @@ return {
           -- render the image inline in the buffer
           -- if your env doesn't support unicode placeholders, this will be disabled
           -- takes precedence over `opts.float` on supported terminals
-          inline = true,
+          inline = false,
           -- render the image in a floating window
           -- only used if `opts.inline` is disabled
           float = true,
