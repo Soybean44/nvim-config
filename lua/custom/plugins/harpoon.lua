eda97781ade600e65abd7fe86d585603569a9c9d
Sovereign Shahid <sovereignshahid@gmail.com>
2025-05-08 10:02:02 -0600

    finished initial development of my compilation-mode plugin

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,104 +1,54 @@
 return {
-  "ThePrimeagen/harpoon",
-  branch = "harpoon2",
-  dependencies = { "nvim-lua/plenary.nvim", "EvWilson/slimux.nvim" },
-  config = function()
-    local harpoon = require "harpoon"
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local harpoon = require "harpoon"
+      harpoon:setup({
+        settings = {
+          sync_on_ui_close = true,
+        },
+      })
 
-    local function run_command(cmd)
-      local function get_os_command_output(input_cmd, cwd)
-        if type(input_cmd) ~= "table" then
-          print("Harpoon: [get_os_command_output]: input_cmd has to be a table")
-          return {}
-        end
-        local Job = require("plenary.job")
-        local command = table.remove(input_cmd, 1)
-        local stderr = {}
-        local stdout, ret = Job
-        :new({
-          command = command,
-          args = input_cmd,
-          cwd = cwd,
-          on_stderr = function(_, data)
-            table.insert(stderr, data)
-          end,
-        })
-        :sync()
-        return stdout, ret, stderr
-      end
-      if string.match(cmd, "$CURRENT_FILE") then
-        -- Get the current buffer ID
-        local buf_id = vim.api.nvim_get_current_buf()
+      vim.keymap.set("n", "<leader>a", function()
+        harpoon:list():add()
+      end)
+      vim.keymap.set("n", "<C-e>", function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end)
+      vim.keymap.set("n", "<M-j>", function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set("n", "<M-f>", function()
+        harpoon:list():select(2)
+      end)
+      vim.keymap.set("n", "<M-k>", function()
+        harpoon:list():select(3)
+      end)
+      vim.keymap.set("n", "<M-d>", function()
+        harpoon:list():select(4)
+      end)
+    end,
+  },
+  {
+    "Soybean44/compilation-mode.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "ThePrimeagen/harpoon" },
+    -- dir = "~/code/lua/compilation-mode.nvim", -- for dev
+    config = function()
+      local compilation_mode = require("compilation-mode")
+      compilation_mode.setup({
+        send_window = "1",
+        send_pane = "0",
+        do_swap = true
+      })
 
-        -- Get the file path of the current buffer
-        local file_path = vim.api.nvim_buf_get_name(buf_id)
-        cmd = string.gsub(cmd, "$CURRENT_FILE", file_path)
-      end
-      local _, ret, stderr = get_os_command_output({
-        "tmux",
-        "send-keys",
-        "-t1",
-        string.format('%s', cmd),
-        ";",
-        "send-keys",
-        "-t1",
-        "Enter",
-        ";",
-        "select-window",
-        "-t1",
-      }, vim.loop.cwd())
-      print(string.format('sending command: %s', harpoon:list("cmd"):get(1).value))
-      if ret ~= 0 then
-        if stderr then
-          error("Failed to send command. " .. stderr[1])
-        end
-      end
+      vim.keymap.set("n", "<M-c>", function()
+        compilation_mode.CompileModeList()
+      end)
+      vim.keymap.set("n", "<M-r>", function()
+        compilation_mode.CompileModeSend()
+      end)
     end
-
-    harpoon:setup({
-      settings = {
-        sync_on_ui_close = true,
-      },
-      ["cmd"] = {
-        select = function(list_item, list, option)
-          run_command(list_item.value)
-        end
-      },
-    })
-
-
-    vim.keymap.set("n", "<leader>a", function()
-      harpoon:list("files"):add()
-    end)
-    vim.api.nvim_create_user_command("ClearCompileCommands", function()
-      harpoon:list("cmd"):clear()
-    end, {})
-    vim.api.nvim_create_user_command("CompileModeCommand", function(opts)
-      print(string.format("Adding command: %s", opts.args))
-      harpoon:list("cmd"):prepend({value = opts.args})
-    end, { nargs='?' })
-    vim.keymap.set("n", "<C-e>", function()
-      harpoon.ui:toggle_quick_menu(harpoon:list("files"))
-    end)
-    vim.keymap.set("n", "<M-c>", function()
-      harpoon.ui:toggle_quick_menu(harpoon:list("cmd"))
-    end)
-
-    vim.keymap.set("n", "<M-j>", function()
-      harpoon:list("files"):select(1)
-    end)
-    vim.keymap.set("n", "<M-f>", function()
-      harpoon:list("files"):select(2)
-    end)
-    vim.keymap.set("n", "<M-k>", function()
-      harpoon:list("files"):select(3)
-    end)
-    vim.keymap.set("n", "<M-d>", function()
-      harpoon:list("files"):select(4)
-    end)
-    vim.keymap.set("n", "<M-r>", function()
-      run_command(harpoon:list("cmd"):get(1).value)
-    end)
-  end,
+  }
 }
-
