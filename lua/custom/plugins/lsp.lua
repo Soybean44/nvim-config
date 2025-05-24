135fdb28c9e13793546fbf6d606c54f04435e7ad
Sovereign Shahid <sovereignshahid@gmail.com>
2025-05-24 13:38:57 -0600

    updated format to be on keybind

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -19,7 +19,6 @@ return {
   },
   {
     "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
     config = function()
       local conform = require "conform"
 
@@ -41,13 +40,6 @@ return {
             prepend_args = { "-A14", "-t" },
           },
         },
-        format_on_save = function(bufnr)
-          -- Disable with a global or buffer-local variable
-          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-            return
-          end
-          return { timeout_ms = 500, lsp_format = "fallback" }
-        end,
       }
       vim.keymap.set({ "n", "v" }, "<leader>mp", function()
         conform.format {
@@ -56,23 +48,6 @@ return {
           timeout_ms = 500,
         }
       end, { desc = "Format file or range (in visual mode)" })
-      vim.api.nvim_create_user_command("FormatDisable", function(args)
-        if args.bang then
-          -- FormatDisable! will disable formatting just for this buffer
-          vim.b.disable_autoformat = true
-        else
-          vim.g.disable_autoformat = true
-        end
-      end, {
-        desc = "Disable autoformat-on-save",
-        bang = true,
-      })
-      vim.api.nvim_create_user_command("FormatEnable", function()
-        vim.b.disable_autoformat = false
-        vim.g.disable_autoformat = false
-      end, {
-        desc = "Re-enable autoformat-on-save",
-      })
     end,
   },
   {
