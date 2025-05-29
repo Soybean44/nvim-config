44550d4193a137f0cebb0b72227d278bda0f73be
Sovereign Shahid <sovereignshahid@gmail.com>
2025-05-28 19:42:25 -0600

    updated latex luasnip plugins so they work

--- lua/custom/plugins/cmp.lua
+++ lua/custom/plugins/cmp.lua
@@ -31,9 +31,7 @@ return {
     "iurimateus/luasnip-latex-snippets.nvim",
     -- vimtex isn't required if using treesitter
     requires = { "L3MON4D3/LuaSnip", "lervag/vimtex" },
-    config = function()
-      require "custom.snippets.latex"
-    end,
+    config = true,
     ft = { "tex", "markdown" },
   },
 }
