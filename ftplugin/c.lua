480d6da255e0fba36ab60d9ab99e4b803ee2735b
Sovereign Shahid <sovereignshahid@gmail.com>
2025-12-01 10:35:33 -0700

    fixed C run bind and added one for quarto

--- ftplugin/c.lua
+++ ftplugin/c.lua
@@ -1,4 +1,4 @@
 vim.treesitter.start()
 
 vim.keymap.del('n', '<leader>r')
-vim.keymap.set('n', '<leader>r', ':make | !tmux next-window && tmux send-keys make Space run Space % Enter<CR><CR>')
+vim.keymap.set('n', '<leader>r', ':make | !tmux next-window && tmux send-keys make Space run Enter<CR><CR>')
---
+++ ftplugin/quarto.lua
@@ -0,0 +1,4 @@
+vim.treesitter.start()
+
+vim.keymap.del('n', '<leader>r')
+vim.keymap.set('n', '<leader>r', '!tmux next-window && tmux send-keys quarto Space preview Space % Enter<CR><CR>')
