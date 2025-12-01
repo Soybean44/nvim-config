1e8a172ea4c98ea2395b63d1aedeac07b019d040
Sovereign Shahid <sovereignshahid@gmail.com>
2025-12-01 10:39:14 -0700

    fixed bug with quarto run in nvim

--- ftplugin/quarto.lua
+++ ftplugin/quarto.lua
@@ -1,2 +1,2 @@
 vim.keymap.del('n', '<leader>r')
-vim.keymap.set('n', '<leader>r', '!tmux next-window && tmux send-keys quarto Space preview Space % Enter<CR><CR>')
+vim.keymap.set('n', '<leader>r', ':!tmux next-window && tmux send-keys quarto Space preview Space % Enter<CR><CR>')
