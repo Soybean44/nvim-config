cee656a26041ad5a5cfed9dfa935d11ca416fa73
Sovereign Shahid <sovereignshahid@gmail.com>
2025-12-13 13:28:07 -0700

    updated typst to use buffer variables to compile specific files only on save

--- ftplugin/typst.lua
+++ ftplugin/typst.lua
@@ -1,20 +1,20 @@
 vim.treesitter.start()
 
-vim.g.maplocalleader = ","
-vim.g.export_typst = true
+vim.b.maplocalleader = ","
+vim.b.export_typst = true
 vim.o.spell = true
 vim.keymap.set('n', '<LocalLeader>tp', ':TypstPreview<CR>')
 
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = "*.typ",
   callback = function ()
-    if vim.g.export_typst then
+    if vim.b.export_typst then
       vim.cmd[[LspTinymistExportPdf]]
     end
   end,
 })
 
 vim.api.nvim_create_user_command("AutoPdfToggle", function ()
-  vim.g.export_typst = not vim.g.export_typst
-  print("Set pdf export to", vim.g.export_typst)
+  vim.b.export_typst = not vim.b.export_typst
+  print("Set pdf export to", vim.b.export_typst)
 end, {})
