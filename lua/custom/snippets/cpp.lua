e6873c0c8e3c8afacc07e6376c9abff409eaa8db
Sovereign Shahid <sovereignshahid@gmail.com>
2026-03-28 18:36:03 -0600

    cleaned up snippets and added a make snippet

--- lua/custom/snippets/cpp.lua
+++ lua/custom/snippets/cpp.lua
@@ -1,21 +1,17 @@
 local ls = require "luasnip"
 -- some shorthands...
 local s = ls.snippet
-local t = ls.text_node
+local fmta = require("luasnip.extras.fmt").fmta
 
 return {
   s({
     trig = "hello",
     namr = "hello world",
     dscr = "hello world program",
-  }, {
-    t {
-      "#include <iostream>",
-      "",
-      "int main (void) {",
-      '  std::cout << "Hello World!\\n";',
-      "  return 0;",
-      "}",
-    },
-  }),
+  }, fmta([[#include <iostream>"
+int main (void) {
+  std::cout << "Hello World!\\n"
+  return 0;
+}]], {})
+),
 }
---
+++ lua/custom/snippets/make.lua
@@ -0,0 +1,31 @@
+local ls = require "luasnip"
+-- some shorthands...
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local c = ls.choice_node
+local fmt = require("luasnip.extras.fmt").fmt
+
+return {
+  s({trig = "make"},fmt(
+   [[CC := gcc
+CFLAGS := -Wall -Wextra -Werror --std=c99
+LDFLAGS := 
+BUILD := build
+SRC := src
+
+.PHONY: all clean run
+
+all: $(BUILD)/main
+
+$(BUILD)/main: $(SRC)/main.c
+	@mkdir -p $(BUILD)
+	$(CC) $(CFLAGS) $< -o $@ $(LDFLAGS)
+
+clean:
+	rm -rf $(BUILD)
+
+run: all
+	./$(BUILD)/main]] , {})),
+}
+
